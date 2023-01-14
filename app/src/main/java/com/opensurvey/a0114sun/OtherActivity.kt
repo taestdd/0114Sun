7ada8d5abc0c2ed4fcf863e237aaea21333014d4
hong <rlaxoxo5252@gmail.com>
2023-01-14 23:47:14 +0900

    use finish()

--- app/src/main/java/com/opensurvey/a0114sun/OtherActivity.kt
+++ app/src/main/java/com/opensurvey/a0114sun/OtherActivity.kt
@@ -1,11 +1,23 @@
 package com.opensurvey.a0114sun
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        returnBtn.setOnClickListener{
+
+//            val returnIntent = Intent(this,MainActivity::class.java)
+//            startActivity(returnIntent)
+
+            finish()
+
+        }
+
     }
 }
